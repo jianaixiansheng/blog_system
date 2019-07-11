81868942c95e9c74b4ae85fd26bc226bce31c090
user2 <user2@163.com>
2019-07-11 19:58:44 +0800

    增加样式

--- body/static/index/js/index.js
+++ body/static/index/js/index.js
@@ -4,7 +4,14 @@ $(function () {
 
     $('.move_font').click(function () {
         $('.move_cont').show(1)
-    })
+    });
+    $('.pub_img1').click(function () {
+        $("[type=file]").click()
+    });
+    $('.text_area').click(function () {
+        $('.text_pub').show(1)
+    });
+    // $('.showall').click()
 
 });
 
